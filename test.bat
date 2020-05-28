889c61144e46d954d4cee31a79722e4e58500604
Martyn <66063147+Martyn555@users.noreply.github.com>
2020-05-28 15:02:20 +0200

    Informatyczny test v.0.6

---
+++ test.bat
@@ -0,0 +1,646 @@
+@chcp 1250
+@echo off
+color f0
+set pytania=0
+set punkty=0
+set proc=0
+set pc=0
+set err=0
+set b1=0
+set baza=0
+set version=0.6
+set testmode=0
+set /p op=<baza.f1n4l
+set /p normal=<resources\%op%\normal.txt
+set /p hard=<resources\%op%\hard.txt
+set /p op=<baza.f1n4l
+set /p forversion=<resources\%op%\forversion.txt
+set /p author=<resources\%op%\author.txt
+title Informatyczny test Martyn's Studio      Wersja: %version%
+setlocal EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+cls
+goto :file
+:text
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+goto :eof
+:file
+goto intro
+:intro
+echo.
+echo.
+echo.
+call :text f1 "        I  N  F  O  R  M  A  T  Y  C  Z  N  Y"
+echo.
+type intro.f1n4l
+timeout /t 4 /nobreak >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo           GNU GENERAL PUBLIC LICENSE
+echo.
+echo.
+echo.
+timeout /t 2 /nobreak >nul
+goto menu
+
+:menu
+cls
+call :text f1 "Informatyczny test Martyn's Studio - Menu"
+echo.
+echo.
+echo 1 - Rozpocznij test
+echo 2 - Wyniki
+echo 3 - Ustawienia baz zadañ
+echo 4 - WyjdŸ
+echo.
+echo 5 - Informacje
+echo 6 - Licencja
+echo 7 - Lista zmian
+choice /n /c:1234567 /M ":"
+if %errorlevel%== 1 goto rules
+if %errorlevel%== 2 goto wyniki1
+if %errorlevel%== 3 goto bza
+if %errorlevel%== 4 goto wyjscie
+if %errorlevel%== 5 goto info
+if %errorlevel%== 6 goto license
+if %errorlevel%== 7 goto listazmian
+goto menu
+
+:listazmian
+cls
+type zmiany.txt
+pause>nul
+goto menu
+
+:license
+cls
+type license.txt
+pause>nul
+goto menu
+
+:info
+cls
+call :text f1 "Informatyczny test Martyn's Studio - Informacje"
+echo.
+echo.
+echo Wersja: %version%
+echo Wydanie na systemy: Windows 10
+echo Autor: Marcin "ZielaRZ" Madej
+echo.
+echo Baza zadañ: %op%
+echo Autor: %author%
+echo Przeznaczona dla wersji: %forversion%
+echo Iloœæ zadañ standardowych: %normal%
+echo Iloœæ zadañ dodatkowych: %hard%
+echo.
+echo W celu zg³aszania b³êdów i sugerowania zmian proszê napisaæ: https://www.messenger.com/t/100010615000573
+pause>nul
+goto menu
+
+
+
+:wyjscie
+cls
+echo Czy na pewno chcesz wyjœæ?
+echo.
+echo 1 - Tak
+echo 2 - Nie
+choice /n /c:12 /M ":"
+if %errorlevel%== 1 exit
+if %errorlevel%== 2 goto menu
+goto wyjscie
+
+:wyniki1
+cls
+echo 1 - Pe³ne wyniki testów
+echo 2 - Ranking
+echo 3 - Wyczyœæ wyniki
+echo 4 - Cofnij
+choice /n /c:1234 /M ":"
+if %errorlevel%== 1 goto wyniki
+if %errorlevel%== 2 goto ranking
+if %errorlevel%== 3 goto wyczysc
+if %errorlevel%== 4 goto menu
+goto wyniki1
+
+
+:wyniki
+cls
+echo Wyniki:
+echo.
+type resources\%op%\wyniki\c.f1n4l
+pause>nul
+goto wyniki1
+
+:wyczysc
+cls
+echo Czy na pewno chcesz wyczyœciæ wyniki?
+echo 1 - Tak
+echo 2 - Nie
+choice /n /c:12 /M ":"
+if %errorlevel%== 1 echo ====================================================== >resources\%op%\wyniki\c.f1n4l
+if %errorlevel%== 1 del resources\%op%\rank\*.txt
+if %errorlevel%== 1 goto dwujka
+if %errorlevel%== 2 goto wyniki1
+goto wyczysc
+:dwujka
+cls
+call :text f2 "Wyniki wyczyszczone."
+pause>nul
+goto wyniki1
+
+:plus1punkt
+cls
+start sound1.vbs
+set /a p1=%punkty% + 1
+set punkty=%p1%
+if %punkty%== 40 goto dodatkowe
+goto random
+
+:random
+cls
+set /a er=%err% + 1
+set err=%er%
+call :text f4 "Trwa losowanie zadania"
+if %pytania%==40 goto ennd
+set /a x=%RANDOM% * %normal% / 32768 + 1
+if not exist resources\%op%\d%x%.f1n4l set err=0
+if %err%== 100 goto error
+if not exist resources\%op%\a%x%.f1n4l goto error
+if not exist resources\%op%\b%x%.txt goto error
+if exist resources\%op%\d%x%.f1n4l goto random
+goto pytanie
+
+:error
+color 9f
+cls
+echo                         B£¥D PODCZAS £ADOWANIA
+echo.
+echo Przepraszamy, wyst¹pi³ b³¹d podczas ³adowania. Za chwilê wyst¹pi automatyczne zresetowanie zmiennych w celu unikniêcia nastêpnych b³êdów.
+echo Prawdopodobnie twoja baza zadañ nie jest pe³na i zawiera mniej ni¿ 40 pytañ. Przeinstaluj j¹, lub zdiagnozuj jeœli b³¹d wyst¹pi ponownie.
+timeout /t 10 /nobreak >nul
+color f0
+set pytania=0
+set punkty=0
+set proc=0
+set pc=0
+set err=0
+set b1=0
+set baza=0
+set /p op=<baza.f1n4l
+del resources\%op%\d*.f1n4l
+del resources\%op%\z*.f1n4l
+goto menu
+
+
+
+
+
+
+:pytanie
+echo TAKIE PYTANIE JU¯ BY£O>resources\%op%\d%x%.f1n4l
+set /a p2=%pytania% + 1
+set pytania=%p2%
+cls
+call :text f1 "                                                                                                            Punkty   %punkty%"
+echo.
+call :text f1 "                                                                                                            Zadanie  %pytania%"
+echo.
+echo.
+echo Treœæ zadania:
+echo.
+type resources\%op%\a%x%.f1n4l
+echo.
+echo.
+echo OdpowiedŸ:
+set /p y=<resources\%op%\b%x%.txt
+set /p odpowiedz=:
+
+if %odpowiedz%== testmode0 set testmode=0
+if %odpowiedz%== testmode0 echo TESTOWANIE WY£¥CZONE
+if %odpowiedz%== testmode0 timeout /t 2 /nobreak >nul
+if %odpowiedz%== testmode1 set testmode=1
+if %odpowiedz%== testmode1 echo TESTOWANIE W£¥CZONE - TYLKO POPRAWNE
+if %odpowiedz%== testmode1 timeout /t 2 /nobreak >nul
+if %odpowiedz%== testmode2 set testmode=2
+if %odpowiedz%== testmode2 echo TESTOWANIE W£¥CZONE - TYLKO FA£SZYWE
+if %odpowiedz%== testmode2 timeout /t 2 /nobreak >nul
+
+if %testmode%== 1 set /p odpowiedz=<resources\%op%\b%x%.txt
+if %testmode%== 2 set odpowiedz=h
+if %odpowiedz%== %y% goto plus1punkt
+start sound2.vbs
+goto random
+
+
+
+
+
+
+
+
+
+:rules
+cls
+call :text f5 "Witaj, %username%."
+echo.
+echo.
+echo Oto zasady testu:
+echo Po wciœniêciu dowolnego klawisza zostanie ci zadane 40 pytañ z dziedziny informatyki.
+echo Test jest na zasadzie wyboru a, b, c, d.
+echo Wciœniêcie entera bez podania odpowiedzi bêdzie uznawane za tak¹ sam¹ odpowiedŸ jak poprzednia.
+echo Odpowiedzi podajemy ma³ymi literami i akceptujemy enterem.
+echo Za ka¿de prawid³owo rozwi¹zane zadanie dostaniesz 1 punkt.
+echo Niskiej tonacji dŸwiêk informuje o b³êdnej odpowiedzi, a wysokiej tonacji o poprawnej.
+echo Po odpowiedzeniu prawid³owo na 40 pytañ zostan¹ ci zadane 3 dodatkowe pytania.
+echo Nie ma dodatkowych punktów za naliczany czas.
+echo Po ukoñczeniu testu mo¿esz sprawdziæ swój czas i wynik, a nastêpnie go zapisaæ lub usun¹æ.
+pause>nul
+del resources\%op%\d*.f1n4l
+del resources\%op%\z*.f1n4l
+set czasrozpoczecia=%time%
+goto random
+
+
+
+
+
+
+
+
+:ennd
+set czaszakonczenia=%time%
+set /p procp=<proc\%punkty%.f1n4l
+cls
+echo Koniec testu, oto wyniki:
+echo.
+echo Czas rozpoczêcia: %czasrozpoczecia%
+echo Czas zakoñczenia: %czaszakonczenia%
+echo Prawid³owe odpowiedzi: %punkty%/40 %procp%
+echo.
+echo Czy chcesz zapisaæ wynik?
+echo 1 - Tak
+echo 2 - Nie
+choice /n /c:12 /M ":"
+if %errorlevel%== 1 goto save
+set pytania=0
+set punkty=0
+set proc=0
+set pc=0
+set err=0
+set b1=0
+set baza=0
+set /p op=<baza.f1n4l
+goto menu
+:save
+cls
+echo Podaj pseudonim.
+set /p nick=:
+echo U¿ytkownik:___________________ %username% >>resources\%op%\wyniki\c.f1n4l
+echo Pseudonim:____________________ %nick% >>resources\%op%\wyniki\c.f1n4l
+echo Czas rozpoczêcia:_____________ %czasrozpoczecia% >>resources\%op%\wyniki\c.f1n4l
+echo Czas zakoñczenia:_____________ %czaszakonczenia% >>resources\%op%\wyniki\c.f1n4l
+echo Prawid³owe odpowiedzi:________ %punkty%/40 >>resources\%op%\wyniki\c.f1n4l
+echo Wynik:________________________ %procp% >>resources\%op%\wyniki\c.f1n4l
+echo ====================================================== >>resources\%op%\wyniki\c.f1n4l
+
+echo %nick% %procp% >resources\%op%\rank\%nick%_%punkty%.txt
+echo ====================================================== >>resources\%op%\rank\%nick%_%punkty%.txt
+cls
+call :text f2 "Wyniki zapisane."
+pause>nul
+set pytania=0
+set punkty=0
+set proc=0
+set pc=0
+set err=0
+set b1=0
+set baza=0
+set /p op=<baza.f1n4l
+set procp=0
+goto menu
+
+
+
+
+:baza
+cls
+echo Baza Zadañ i odpowiedzi:
+echo.
+echo ==========================================================================================================================================
+echo.
+goto bazaa
+:bazaa
+set /a b1=%baza% + 1
+set baza=%b1%
+if not exist resources\%op%\a%baza%.f1n4l goto dddd
+echo Zadanie NR.%baza%:
+echo.
+type resources\%op%\a%baza%.f1n4l
+echo.
+echo.
+echo OdpowiedŸ:
+type resources\%op%\b%baza%.txt
+echo.
+echo.
+echo ==========================================================================================================================================
+echo.
+goto bazaa
+:dddd
+set baza=0
+set b1=0
+goto dddd1
+:dddd1
+set /a b1=%baza% + 1
+set baza=%b1%
+if not exist resources\%op%\o%baza%.f1n4l pause>nul
+if not exist resources\%op%\o%baza%.f1n4l goto menu
+echo Zadanie dodatkowe NR.%baza%:
+echo.
+type resources\%op%\o%baza%.f1n4l
+echo.
+echo.
+echo OdpowiedŸ:
+type resources\%op%\i%baza%.txt
+echo.
+echo.
+echo ==========================================================================================================================================
+echo.
+goto dddd1
+
+
+
+
+
+
+
+
+
+
+:dodatkowe
+start sound3.vbs
+cls
+echo Brawo, znasz odpowiedzi na wszystkie zadanie ci do tej pory pytania!
+echo Po wciœniêciu dowolnego klawisza zostan¹ ci zadane 3 kolejne.
+pause>nul
+goto randomb
+
+:randomb
+cls
+set /a er=%err% + 1
+set err=%er%
+call :text f4 "Trwa losowanie zadania"
+if %pytania%==43 goto ennd
+set /a x=%RANDOM% * %hard% / 32768 + 1
+if not exist resources\%op%\z%x%.f1n4l set err=0
+if %err%== 100 goto error
+if not exist resources\%op%\o%x%.f1n4l goto error
+if not exist resources\%op%\i%x%.txt goto error
+if exist resources\%op%\z%x%.f1n4l goto randomb
+goto pytanieb
+
+
+:pytanieb
+echo TAKIE PYTANIE JU¯ BY£O>resources\%op%\z%x%.f1n4l
+set /a p2=%pytania% + 1
+set pytania=%p2%
+cls
+echo.
+echo.
+echo Treœæ zadania:
+echo.
+type resources\%op%\o%x%.f1n4l
+echo.
+echo.
+echo OdpowiedŸ:
+set /p y=<resources\%op%\i%x%.txt
+set /p odpowiedz=:
+
+if %odpowiedz%== testmode0 set testmode=0
+if %odpowiedz%== testmode0 echo TESTOWANIE WY£¥CZONE
+if %odpowiedz%== testmode0 timeout /t 2 /nobreak >nul
+if %odpowiedz%== testmode1 set testmode=1
+if %odpowiedz%== testmode1 echo TESTOWANIE W£¥CZONE - TYLKO POPRAWNE
+if %odpowiedz%== testmode1 timeout /t 2 /nobreak >nul
+if %odpowiedz%== testmode2 set testmode=2
+if %odpowiedz%== testmode2 echo TESTOWANIE W£¥CZONE - TYLKO FA£SZYWE
+if %odpowiedz%== testmode2 timeout /t 2 /nobreak >nul
+
+if %testmode%== 1 set /p odpowiedz=<resources\%op%\i%x%.txt
+if %testmode%== 2 set odpowiedz=h
+if %odpowiedz%== %y% goto plus1punktb
+start sound2.vbs
+goto randomb
+
+
+:plus1punktb
+cls
+start sound1.vbs
+set /a p1=%punkty% + 1
+set punkty=%p1%
+goto randomb
+
+
+
+
+
+
+
+
+
+:bza
+cls
+echo Ustawienia - bazy zadañ
+echo Aktualnie wybrana baza: %op%
+echo.
+echo 1 - Wyœwietl zawartoœæ wybranej bazy zadañ
+echo 2 - Zmieñ bazê zadañ
+echo 3 - Diagnozuj wybran¹ bazê zadañ
+echo 4 - Wyœwietl logi poprzedniej diagnozy
+echo 5 - Cofnij
+choice /n /c:12345 /M ":"
+if %errorlevel%== 1 goto baza
+if %errorlevel%== 2 goto usbaza
+if %errorlevel%== 3 goto diag
+if %errorlevel%== 4 goto podiag
+if %errorlevel%== 5 goto menu
+goto bza
+
+:usbaza
+cls
+echo Wpisz nazwê katalogu z baz¹ zadañ.
+set /p secik=:
+echo %secik%>baza.f1n4l
+set /p op=<baza.f1n4l
+set /p normal=<resources\%op%\normal.txt
+set /p hard=<resources\%op%\hard.txt
+set /p forversion=<resources\%op%\forversion.txt
+set /p author=<resources\%op%\author.txt
+goto bza
+
+
+
+
+
+
+
+
+
+:diag
+cls
+echo Baza zadañ %op% bêdzie za chwilê diagnozowania.
+echo Nie przerywaj operacji, program sam poinformuje ciê o zakoñczeniu pracy.
+echo Mo¿e to potrwaæ kilka minut.
+timeout /t 10 /nobreak >nul
+cls
+echo %time:~0,5% Diagnostyka z %date% >resources\%op%\log.f1n4l
+set brr=0
+set brak=0
+goto diag12
+
+:diag12
+cls
+echo %time% Sprawdzanie pliku odpowiedzialnego za iloœæ zadañ (1/1)
+if exist resources\%op%\normal.txt echo %time% Plik odpowiedzialny za iloœæ zadañ znajduje siê w bazie (1/1) >>resources\%op%\log.f1n4l
+if not exist resources\%op%\normal.txt echo %time% Brakuje pliku odpowiedzialnego za iloœæ zadañ (1/1) >>resources\%op%\log.f1n4l
+if not exist resources\%op%\normal.txt echo %time% Brakuje istotnego pliku, bez którego nie da siê przeprowadziæ dalszej diagnozy.>>resources\%op%\log.f1n4l
+if not exist resources\%op%\normal.txt goto podiag
+echo %time% Sprawdzanie pliku odpowiedzialnego za iloœæ dodatkowych zadañ (1/1)
+if not exist resources\%op%\hard.txt echo %time% Brakuje pliku odpowiedzialnego za iloœæ dodatkowych zadañ (1/1) >>resources\%op%\log.f1n4l
+if exist resources\%op%\hard.txt echo %time% Plik odpowiedzialny za iloœæ dodatkowych zadañ znajduje siê w bazie (1/1) >>resources\%op%\log.f1n4l
+if not exist resources\%op%\hard.txt echo %time% Brakuje istotnego pliku, bez którego nie da siê przeprowadziæ dalszej diagnozy.>>resources\%op%\log.f1n4l
+if not exist resources\%op%\hard.txt goto podiag
+goto diag1
+
+:diag1
+set /a brak=%brr% + 1
+set brr=%brak%
+echo %time% Sprawdzanie braków zadañ. (%brr% / %normal%)
+if exist resources\%op%\a%brr%.f1n4l echo %time% Pytanie %brr% znajduje siê w bazie>>resources\%op%\log.f1n4l
+if not exist resources\%op%\a%brr%.f1n4l echo %time% Brakuje zadania %brr%>>resources\%op%\log.f1n4l
+if %brr%== %normal% goto diag15
+goto diag1
+
+:diag15
+set brr=0
+set brak=0
+goto diag2
+
+:diag2
+set /a brak=%brr% + 1
+set brr=%brak%
+echo %time% Sprawdzanie braków odpowiedzi. (%brr% / %normal%)
+if exist resources\%op%\b%brr%.txt echo %time% OdpowiedŸ %brr% znajduje siê w bazie>>resources\%op%\log.f1n4l
+if not exist resources\%op%\b%brr%.txt echo %time% Brakuje odpowiedzi %brr%>>resources\%op%\log.f1n4l
+if %brr%== %normal% goto diag25
+goto diag2
+
+:diag25
+set brr=0
+set brak=0
+goto diag3
+
+:diag3
+set /a brak=%brr% + 1
+set brr=%brak%
+echo %time% Sprawdzanie braków zadañ dodatkowych. (%brr% / %hard%)
+if exist resources\%op%\o%brr%.f1n4l echo %time% Zadanie dodatkowe %brr% znajduje siê w bazie>>resources\%op%\log.f1n4l
+if not exist resources\%op%\o%brr%.f1n4l echo %time% Brakuje zadania dodatkowego %brr%>>resources\%op%\log.f1n4l
+if %brr%== %hard% goto diag35
+goto diag3
+
+:diag35
+set brr=0
+set brak=0
+goto diag4
+
+:diag4
+set /a brak=%brr% + 1
+set brr=%brak%
+echo %time% Sprawdzanie braków odpowiedzi dodatkowych. (%brr% / %hard%)
+if exist resources\%op%\i%brr%.txt echo %time% OdpowiedŸ dodatkowa %brr% znajduje siê w bazie>>resources\%op%\log.f1n4l
+if not exist resources\%op%\i%brr%.txt echo %time% Brakuje odpowiedzi dodatkowej %brr%>>resources\%op%\log.f1n4l
+if %brr%== %hard% goto diag45
+goto diag4
+
+:diag45
+set brr=0
+set brak=0
+goto diag5
+
+:diag5
+set /a brak=%brr% + 1
+set brr=%brak%
+echo %time% Sprawdzanie minimalnej iloœci zadañ. (%brr% / 40)
+if %brr%== 40 echo %time% Minimalna iloœæ zadañ: %brr% / 40>>resources\%op%\log.f1n4l
+if not exist resources\%op%\a%brr%.f1n4l echo %time% Minimalna iloœæ zadañ: %brr% / 40>>resources\%op%\log.f1n4l
+if not exist resources\%op%\a%brr%.f1n4l goto diag6
+if %brr%== 40 goto diag55
+goto diag5
+
+:diag55
+set brr=0
+set brak=0
+goto diag6
+
+:diag6
+set /a brak=%brr% + 1
+set brr=%brak%
+echo %time% Sprawdzanie minimalnej iloœci dodatkowych zadañ. (%brr% / 3)
+if %brak%== 3 echo %time% Minimalna iloœæ dodatkowych zadañ: %brr% / 3 >>resources\%op%\log.f1n4l
+if not exist resources\%op%\o%brr%.f1n4l echo %time% Minimalna iloœæ dodatkowych zadañ: %brr% / 3>>resources\%op%\log.f1n4l
+if not exist resources\%op%\o%brr%.f1n4l goto diag6
+if %brr%== 3 goto diag7
+goto diag6
+
+
+:diag7
+if %forversion%== %version% echo %time% Wersja Bazy pokrywa siê z wersj¹ testu.>>resources\%op%\log.f1n4l
+if not %forversion%== %version% echo %time% Wersja Bazy nie jest przeznaczona dla tej wersji testu.>>resources\%op%\log.f1n4l
+cls
+echo %time% Diagnostyka bazy zadañ zosta³a zakoñczona.
+echo Po wciœniêciu dowolnego klawisza zobaczysz wyniki.
+pause>nul
+cls
+goto podiag
+
+:podiag
+cls
+type resources\%op%\log.f1n4l
+pause>nul
+goto bza
+
+
+
+
+
+
+:ranking
+set r1=0
+set ranking=0
+cls
+echo Ranking:
+echo.
+echo ======================================================
+goto r
+
+:r2
+set /a r1=%ranking% + 1
+set ranking=%r1%
+goto r
+
+:r
+if exist resources\%op%\rank\*_%r1%.txt type resources\%op%\rank\*_%r1%.txt
+if %r1%== 43 pause>nul
+if %r1%== 43 goto wyniki1
+goto r2
